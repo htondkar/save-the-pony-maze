de6aea3275dbeb2c316a9494fcd83afcb3c3deb6
Hamid Tondkaran <hamid.tondkaranfar@tradeshift.com>
2020-01-20 11:47:10 +0100

    improve home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,71 +1,78 @@
-import { observer } from 'mobx-react';
-import React, { useContext, useState } from 'react';
-import { Redirect } from 'react-router-dom';
-import styled from 'styled-components';
-
-import { AppContext } from '../App';
+import { observer } from 'mobx-react'
+import React, { useContext, useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import styled from 'styled-components'
+import { AppContext } from '../App'
+import { Typography, TextField, Button } from '@material-ui/core'
 
 export const Home = observer(function Home() {
-  const [columns, setColumns] = useState('15')
-  const [rows, setRows] = useState('15')
-  const [difficulty, setDifficulty] = useState('0')
+	const [columns, setColumns] = useState('15')
+	const [rows, setRows] = useState('15')
+	const [difficulty, setDifficulty] = useState('0')
 
-  const context = useContext(AppContext)
+	const context = useContext(AppContext)
 
-  function handleClick() {
-    context.startTheGame({ columns, rows, difficulty })
-  }
+	function handleClick() {
+		context.startTheGame({ columns, rows, difficulty })
+	}
 
-  function extractDomValue(event: React.ChangeEvent<HTMLInputElement>) {
-    return event.target.value
-  }
+	function extractDomValue(event: any) {
+		return event.target.value
+	}
 
-  if (context.mazeId) {
-    return <Redirect to={`/maze/${context.mazeId}`} />
-  }
+	if (context.mazeId) {
+		return <Redirect to={`/maze/${context.mazeId}`} />
+	}
 
-  return (
-    <HomeWrapper>
-      <Input
-        type="number"
-        onChange={e => setColumns(extractDomValue(e))}
-        value={columns}
-        placeholder="columns"
-      ></Input>
-      <Input
-        type="number"
-        onChange={e => setRows(extractDomValue(e))}
-        value={rows}
-        placeholder="rows"
-      ></Input>
-      <Input
-        type="number"
-        onChange={e => setDifficulty(extractDomValue(e))}
-        value={difficulty}
-        placeholder="difficulty"
-      ></Input>
-      <Button onClick={handleClick} disabled={context.isLoading}>
-        Start
-      </Button>
-    </HomeWrapper>
-  )
+	return (
+		<HomeWrapper>
+			<Typography variant="h4">New Game</Typography>
+			<Input
+				type="number"
+				onChange={e => setColumns(extractDomValue(e))}
+				value={columns}
+				placeholder="columns"
+				label="columns"
+				variant="outlined"
+			></Input>
+			<Input
+				type="number"
+				onChange={e => setRows(extractDomValue(e))}
+				value={rows}
+				placeholder="rows"
+				label="rows"
+				variant="outlined"
+			></Input>
+			<Input
+				type="number"
+				onChange={e => setDifficulty(extractDomValue(e))}
+				value={difficulty}
+				placeholder="difficulty"
+				label="difficulty"
+				variant="outlined"
+			></Input>
+
+			<Button
+				variant="contained"
+				color="primary"
+				onClick={handleClick}
+				disabled={context.isLoading}
+				size="large"
+			>
+				Start
+			</Button>
+		</HomeWrapper>
+	)
 })
 
-const Input = styled.input`
-  padding: 5px;
+const Input = styled(TextField)`
+	font-size: 20px;
 `
-const Button = styled.button`
-  padding: 5px;
-  grid-column: 1 / -1;
-  :disabled {
-    opacity: 0.5;
-  }
-`
+
 const HomeWrapper = styled.div`
-  padding: 30%;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  justify-content: center;
-  align-items: center;
+	display: grid;
+	grid-gap: 10px;
+	grid-auto-flow: row;
+	justify-content: center;
+	align-items: center;
 `
